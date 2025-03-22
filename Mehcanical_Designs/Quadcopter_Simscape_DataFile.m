16a42d176aadcd0b45fadde1969a563619a00fdb
Auza-Banegas-Guillermo-J <142273820+Auza-Banegas-Guillermo-J@users.noreply.github.com>
2025-03-22 18:48:23 -0400

    Add files via upload

---
+++ Mehcanical_Designs/Quadcopter_Simscape_DataFile.m
@@ -0,0 +1,372 @@
+% Simscape(TM) Multibody(TM) version: 24.1
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(33).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(33).angle = 0.0;
+smiData.RigidTransform(33).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(33).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [-1.0547118733938987e-12 9.5250000000000057 304.80000000000007];  % mm
+smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(1).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[Frame-1:-:Drone Motor-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [1.0914907460080081e-14 -1.1316166252878643e-13 5.6843418860808015e-14];  % mm
+smiData.RigidTransform(2).angle = 0;  % rad
+smiData.RigidTransform(2).axis = [0 0 0];
+smiData.RigidTransform(2).ID = "F[Frame-1:-:Drone Motor-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [-304.80000000000001 9.5250000000000057 0];  % mm
+smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(3).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[Frame-1:-:Drone Motor-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [3.4086577318405104e-15 1.0014124778032764e-15 1.9721522630525295e-31];  % mm
+smiData.RigidTransform(4).angle = 1.3877787807814457e-16;  % rad
+smiData.RigidTransform(4).axis = [0.99999952413486459 0.00097556652479147132 6.7693493904326033e-20];
+smiData.RigidTransform(4).ID = "F[Frame-1:-:Drone Motor-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [304.79999999999995 9.5250000000000057 0];  % mm
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(5).ID = "B[Frame-1:-:Drone Motor-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [0 1.2621774483536189e-29 5.6843418860808027e-14];  % mm
+smiData.RigidTransform(6).angle = 0;  % rad
+smiData.RigidTransform(6).axis = [0 0 0];
+smiData.RigidTransform(6).ID = "F[Frame-1:-:Drone Motor-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [3.219646771412954e-12 9.5250000000000057 -304.80000000000001];  % mm
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(7).ID = "B[Frame-1:-:Drone Motor-7]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-1.9157230871334016e-14 2.0995096386745242e-14 5.6843418860808027e-14];  % mm
+smiData.RigidTransform(8).angle = 1.1102230246251565e-16;  % rad
+smiData.RigidTransform(8).axis = [0.4037108252396292 0.91488664302433542 2.0503023815180903e-17];
+smiData.RigidTransform(8).ID = "F[Frame-1:-:Drone Motor-7]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [0 0 69.849999999999994];  % mm
+smiData.RigidTransform(9).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(9).axis = [1 0 0];
+smiData.RigidTransform(9).ID = "B[Drone Motor-1:-:Drone fan-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [-1.1938313492289269e-13 -12.700000000000017 -4.3629559187650821e-14];  % mm
+smiData.RigidTransform(10).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(10).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
+smiData.RigidTransform(10).ID = "F[Drone Motor-1:-:Drone fan-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [0 0 69.849999999999994];  % mm
+smiData.RigidTransform(11).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(11).axis = [1 0 0];
+smiData.RigidTransform(11).ID = "B[Drone Motor-5:-:Drone fan-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [8.6681826290472926e-15 -12.700000000000017 -1.1335589830728062e-13];  % mm
+smiData.RigidTransform(12).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(12).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
+smiData.RigidTransform(12).ID = "F[Drone Motor-5:-:Drone fan-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [0 0 69.850000000000023];  % mm
+smiData.RigidTransform(13).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(13).axis = [1 0 0];
+smiData.RigidTransform(13).ID = "B[Drone Motor-7:-:Drone fan-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [-4.9644971645289355e-14 -12.700000000000102 6.3227567768410515e-14];  % mm
+smiData.RigidTransform(14).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(14).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(14).ID = "F[Drone Motor-7:-:Drone fan-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [0 0 69.849999999999966];  % mm
+smiData.RigidTransform(15).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(15).axis = [1 0 0];
+smiData.RigidTransform(15).ID = "B[Drone Motor-3:-:Drone fan-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [-2.3911387237618718e-13 -12.699999999999392 -2.8734729858659243e-13];  % mm
+smiData.RigidTransform(16).angle = 2.0943951023931962;  % rad
+smiData.RigidTransform(16).axis = [-0.57735026918962606 -0.57735026918962651 -0.57735026918962473];
+smiData.RigidTransform(16).ID = "F[Drone Motor-3:-:Drone fan-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [0 0 0];  % mm
+smiData.RigidTransform(17).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(17).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(17).ID = "B[Drone fan-1:-:Drone fin cap-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [-2.1953580716617578e-13 0 1.2816986669104787e-13];  % mm
+smiData.RigidTransform(18).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(18).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962562];
+smiData.RigidTransform(18).ID = "F[Drone fan-1:-:Drone fin cap-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(19).translation = [0 0 0];  % mm
+smiData.RigidTransform(19).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(19).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(19).ID = "B[Drone fan-6:-:Drone fin cap-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(20).translation = [3.8253333937976859e-14 6.5369931689929217e-13 3.8430221377787662e-13];  % mm
+smiData.RigidTransform(20).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(20).axis = [0.57735026918962562 -0.57735026918962506 0.57735026918962662];
+smiData.RigidTransform(20).ID = "F[Drone fan-6:-:Drone fin cap-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(21).translation = [0 0 0];  % mm
+smiData.RigidTransform(21).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(21).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(21).ID = "B[Drone fan-4:-:Drone fin cap-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(22).translation = [1.0229228705517034e-13 -5.684341886080804e-14 7.5446400496968412e-14];  % mm
+smiData.RigidTransform(22).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(22).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(22).ID = "F[Drone fan-4:-:Drone fin cap-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(23).translation = [0 0 0];  % mm
+smiData.RigidTransform(23).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(23).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(23).ID = "B[Drone fan-2:-:Drone fin cap-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(24).translation = [-1.1344829620467989e-13 2.8421709430404007e-14 7.3607852431841821e-15];  % mm
+smiData.RigidTransform(24).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(24).axis = [0.57735026918962584 -0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(24).ID = "F[Drone fan-2:-:Drone fin cap-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(25).translation = [-25.399999999999977 43.179999999999993 -57.149999999999977];  % mm
+smiData.RigidTransform(25).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(25).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(25).ID = "B[Frame-1:-:Drone Cam support-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(26).translation = [0 -5.6843418860808015e-14 -1.2621774483536189e-29];  % mm
+smiData.RigidTransform(26).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(26).axis = [-1 -0 -0];
+smiData.RigidTransform(26).ID = "F[Frame-1:-:Drone Cam support-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(27).translation = [25.399999999999977 43.179999999999993 -57.149999999999977];  % mm
+smiData.RigidTransform(27).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(27).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(27).ID = "B[Frame-1:-:Drone Cam support-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(28).translation = [4.5112470673449665e-14 -6.6537309273126401e-14 0];  % mm
+smiData.RigidTransform(28).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(28).axis = [-1 8.6277264527055214e-34 -1.1850744973157927e-17];
+smiData.RigidTransform(28).ID = "F[Frame-1:-:Drone Cam support-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(29).translation = [25.399999999999977 43.179999999999993 57.150000000000034];  % mm
+smiData.RigidTransform(29).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(29).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(29).ID = "B[Frame-1:-:Drone Cam support-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(30).translation = [0 0 0];  % mm
+smiData.RigidTransform(30).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(30).axis = [-1 1.5336351980574125e-33 -2.348870180924853e-17];
+smiData.RigidTransform(30).ID = "F[Frame-1:-:Drone Cam support-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(31).translation = [-25.399999999999977 43.179999999999993 57.150000000000034];  % mm
+smiData.RigidTransform(31).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(31).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(31).ID = "B[Frame-1:-:Drone Cam support-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(32).translation = [0 0 0];  % mm
+smiData.RigidTransform(32).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(32).axis = [1 -7.0103849975695492e-33 -4.3444347681827388e-17];
+smiData.RigidTransform(32).ID = "F[Frame-1:-:Drone Cam support-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(33).translation = [288.04406038721453 268.29251341418751 524.66487377569285];  % mm
+smiData.RigidTransform(33).angle = 0;  % rad
+smiData.RigidTransform(33).axis = [0 0 0];
+smiData.RigidTransform(33).ID = "RootGround[Frame-1]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(5).mass = 0.0;
+smiData.Solid(5).CoM = [0.0 0.0 0.0];
+smiData.Solid(5).MoI = [0.0 0.0 0.0];
+smiData.Solid(5).PoI = [0.0 0.0 0.0];
+smiData.Solid(5).color = [0.0 0.0 0.0];
+smiData.Solid(5).opacity = 0.0;
+smiData.Solid(5).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.0041134388840899988;  % kg
+smiData.Solid(1).CoM = [0 7.2114578313253004 0];  % mm
+smiData.Solid(1).MoI = [0.16459821344187511 0.17406158481293837 0.16459821344187511];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(1).color = [0.69411764705882351 0.098039215686274508 0.098039215686274508];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "Drone fin cap*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.041414473880984813;  % kg
+smiData.Solid(2).CoM = [0 3.986597280669685 9.4615720847600329e-13];  % mm
+smiData.Solid(2).MoI = [275.54632085066163 281.56793291816473 7.9897859414246621];  % kg*mm^2
+smiData.Solid(2).PoI = [0 -24.501292501490475 0];  % kg*mm^2
+smiData.Solid(2).color = [0.69411764705882351 0.098039215686274508 0.098039215686274508];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "Drone fan*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.07103183380185045;  % kg
+smiData.Solid(3).CoM = [0 0 21.904759173772096];  % mm
+smiData.Solid(3).MoI = [21.202962717956666 21.202962717956666 18.250055949775579];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.29803921568627451 0.29803921568627451 0.29803921568627451];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "Drone Motor*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 4.9258813644325548;  % kg
+smiData.Solid(4).CoM = [1.1051176684627055 11.400725925752765 0.46152902824829717];  % mm
+smiData.Solid(4).MoI = [48121.653060064142 88777.611849611741 48691.795236635371];  % kg*mm^2
+smiData.Solid(4).PoI = [28.25206807703551 1.6380057107069632 67.093348508072552];  % kg*mm^2
+smiData.Solid(4).color = [1 1 1];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "Frame*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.010762203368141722;  % kg
+smiData.Solid(5).CoM = [0 0 25.399999999999999];  % mm
+smiData.Solid(5).MoI = [2.6212132867710816 2.6212132867710816 0.61353115688195659];  % kg*mm^2
+smiData.Solid(5).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(5).color = [0.29803921568627451 0.29803921568627451 0.29803921568627451];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = "Drone Cam support*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(16).Rz.Pos = 0.0;
+smiData.RevoluteJoint(16).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = -95.509370325364017;  % deg
+smiData.RevoluteJoint(1).ID = "[Frame-1:-:Drone Motor-1]";
+
+smiData.RevoluteJoint(2).Rz.Pos = -106.37199641505413;  % deg
+smiData.RevoluteJoint(2).ID = "[Frame-1:-:Drone Motor-3]";
+
+smiData.RevoluteJoint(3).Rz.Pos = -118.82114836023034;  % deg
+smiData.RevoluteJoint(3).ID = "[Frame-1:-:Drone Motor-5]";
+
+smiData.RevoluteJoint(4).Rz.Pos = -132.37926797120815;  % deg
+smiData.RevoluteJoint(4).ID = "[Frame-1:-:Drone Motor-7]";
+
+smiData.RevoluteJoint(5).Rz.Pos = -11.999170461871529;  % deg
+smiData.RevoluteJoint(5).ID = "[Drone Motor-1:-:Drone fan-1]";
+
+smiData.RevoluteJoint(6).Rz.Pos = -24.448322407047716;  % deg
+smiData.RevoluteJoint(6).ID = "[Drone Motor-5:-:Drone fan-2]";
+
+smiData.RevoluteJoint(7).Rz.Pos = 40.758998176197572;  % deg
+smiData.RevoluteJoint(7).ID = "[Drone Motor-7:-:Drone fan-4]";
+
+smiData.RevoluteJoint(8).Rz.Pos = -176.12564400174529;  % deg
+smiData.RevoluteJoint(8).ID = "[Drone Motor-3:-:Drone fan-6]";
+
+smiData.RevoluteJoint(9).Rz.Pos = -116.66781950441647;  % deg
+smiData.RevoluteJoint(9).ID = "[Drone fan-1:-:Drone fin cap-1]";
+
+smiData.RevoluteJoint(10).Rz.Pos = 47.458654035457265;  % deg
+smiData.RevoluteJoint(10).ID = "[Drone fan-6:-:Drone fin cap-2]";
+
+smiData.RevoluteJoint(11).Rz.Pos = 177.01589224653662;  % deg
+smiData.RevoluteJoint(11).ID = "[Drone fan-4:-:Drone fin cap-4]";
+
+smiData.RevoluteJoint(12).Rz.Pos = -90.660547948262462;  % deg
+smiData.RevoluteJoint(12).ID = "[Drone fan-2:-:Drone fin cap-6]";
+
+smiData.RevoluteJoint(13).Rz.Pos = -89.999999999999986;  % deg
+smiData.RevoluteJoint(13).ID = "[Frame-1:-:Drone Cam support-1]";
+
+smiData.RevoluteJoint(14).Rz.Pos = -100.8626260896901;  % deg
+smiData.RevoluteJoint(14).ID = "[Frame-1:-:Drone Cam support-2]";
+
+smiData.RevoluteJoint(15).Rz.Pos = -113.31177803486631;  % deg
+smiData.RevoluteJoint(15).ID = "[Frame-1:-:Drone Cam support-4]";
+
+smiData.RevoluteJoint(16).Rz.Pos = -126.86989764584411;  % deg
+smiData.RevoluteJoint(16).ID = "[Frame-1:-:Drone Cam support-6]";
+
