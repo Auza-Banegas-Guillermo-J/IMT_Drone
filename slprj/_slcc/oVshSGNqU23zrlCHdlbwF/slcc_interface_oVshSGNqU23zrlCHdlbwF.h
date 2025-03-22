59934256c177485ea2f21b423d82ab3451cc8668
Auza-Banegas-Guillermo-J <142273820+Auza-Banegas-Guillermo-J@users.noreply.github.com>
2025-03-22 18:49:32 -0400

    Add files via upload

---
+++ slprj/_slcc/oVshSGNqU23zrlCHdlbwF/slcc_interface_oVshSGNqU23zrlCHdlbwF.h
@@ -0,0 +1,26 @@
+#include "customcode_oVshSGNqU23zrlCHdlbwF.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+DLL_EXPORT_CC extern void get_motor_speeds_addr_oVshSGNqU23zrlCHdlbwF(real_T (**varAddr)[4]);
+DLL_EXPORT_CC extern void get_attitude_addr_oVshSGNqU23zrlCHdlbwF(real_T (**varAddr)[4]);
+DLL_EXPORT_CC extern const char_T *get_dll_checksum_oVshSGNqU23zrlCHdlbwF(void);
+DLL_EXPORT_CC extern void calc_motors_oVshSGNqU23zrlCHdlbwF(void);
+
+/* Function Definitions */
+DLL_EXPORT_CC const uint8_T *get_checksum_source_info(int32_T *size);
+#ifdef __cplusplus
+}
+#endif
+
